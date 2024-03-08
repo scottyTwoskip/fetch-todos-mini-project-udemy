66c7c6f4ad32ad0a124e5cfd0cdcb02614330acb
scottyTwoskip <scottyeansor@gmail.com>
2024-03-08 11:06:17 -0700

    finished moc todo list

--- typicode-todos/script.js
+++ typicode-todos/script.js
@@ -22,4 +22,29 @@ const addToDoToDOM = (toDo) => {
 
     document.getElementById('todo-list').appendChild(div)
 }
-getToDos()
+
+const createToDo = (e) => {
+    e.preventDefault()
+
+    const newToDo = {
+        title: e.target.firstElementChild.value,
+        completed: false
+    }
+
+    fetch(apiUrl, {
+        method: 'POST',
+        body: JSON.stringify(newToDo),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => addToDoToDOM(data))
+}
+
+const init = () => {
+    document.addEventListener('DOMContentLoaded', getToDos)
+    document.querySelector('#todo-form').addEventListener('submit', createToDo)
+}
+
+init()
